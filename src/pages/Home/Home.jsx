9b13782ee1d4e6be255cbe6e30d5009134a87ea1
KathyP825 <kp2327@nyu.edu>
2022-02-02 21:28:29 -0500

    relabeled for Hotspots, added buttons for SignUp/Login

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,22 +10,44 @@ export default function Home(){
 
   return (
     <div className="content">
-      <h1>🤗 SWE React Demo </h1>
+      <h1>Hotspots</h1>
       <div>
-        <p className="quote">"A list is only as strong as its weakest link"</p>
-        <p className="quote-author">- Donald Knuth</p>
+        <p className="quote">"Crowdsourcing study spaces for students by students"</p>
+        <hr></hr>
+        <hr></hr>
+        <p className="dev-names">Tiffany Chan, Kevin Chen, Kevin Iza, Kathy Pan</p>
       </div>
+
+      { 
+        // button for Locations
+      }
       <button
         onClick={() => navigateToPage('/rooms')}
         className="page-button"
       >
-        View All Rooms
+        View All Locations
       </button>
+
+      { 
+        // button for User login?
+      }
       <button
         onClick={() => navigateToPage('/users')}
         className="page-button"
       >
-        View All Users
+        User Login
+      </button> 
+
+      { /*
+        - button for User signup?
+        - maybe we can split the pages where the user logs in and signs up
+        - currently routes to the same place as User Login
+      */}
+      <button
+        onClick={() => navigateToPage('/users')}
+        className="page-button"
+      >
+        User SignUp
       </button> 
     </div>
   );
