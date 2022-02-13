9c3a269110ba93999288b30197a173b1323c3956
tiffanychan0909 <55333047+tiffanychan0909@users.noreply.github.com>
2022-02-13 15:44:59 -0500

    make spotcard href

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,25 +4,30 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
+import {useHistory} from 'react-router-dom';
 
-export default function SpotCard({component,height,image}) {
-  return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-         component={component}
-         height={height}
-         image={image}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Campus Spot
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam a dolor gravida, aliquam tellus ut, porttitor risus.
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-  );
+export default function SpotCard({component,height,image,hpath}) {
+    const history = useHistory();
+    function navigateToPage(path) {
+        history.push(path);
+    }
+    return (
+        <Card sx={{ maxWidth: 400}}>
+        <CardActionArea>
+            <CardMedia
+            component={component}
+            height={height}
+            image={image}
+            />
+            <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+                Campus Spot
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam a dolor gravida, aliquam tellus ut, porttitor risus.
+            </Typography>
+            </CardContent>
+        </CardActionArea>
+        </Card>
+    );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import {useHistory} from 'react-router-dom';
+import Card from '@mui/material/Card';
+import { CardActionArea } from '@mui/material';
 import SpotCard from '../../components/Card/Card';
 
 export default function Home(){
   const [component, setComponent] = useState("img");
   const [height, setHeight] = React.useState("140");
   const image = require('./images/nyu.jpg');
+  const [path, setPath] = useState('/rooms')
   const history = useHistory();
 
   function navigateToPage(path) {
@@ -34,36 +37,54 @@ export default function Home(){
         </button>
       </div>
       <div className="grid-container">
-        <SpotCard
-          component={component}
-          height={height}
-          image={image}
-        />
-        <SpotCard
-          component={component}
-          height={height}
-          image={image}
-        />
-        <SpotCard
-          component={component}
-          height={height}
-          image={image}
-        />
-        <SpotCard
-          component={component}
-          height={height}
-          image={image}
-        />
-        <SpotCard
-          component={component}
-          height={height}
-          image={image}
-        />
-        <SpotCard
-          component={component}
-          height={height}
-          image={image}
-        />
+        <div className="grid-item" onClick={() => navigateToPage('/rooms')}>
+          <SpotCard
+            component={component}
+            height={height}
+            image={image}
+            onClick={() => navigateToPage('/rooms')} // todo change path
+          />
+        </div>
+        <div className="grid-item" onClick={() => navigateToPage('/rooms')}>
+          <SpotCard
+            component={component}
+            height={height}
+            image={image}
+            onClick={() => navigateToPage('/rooms')} // todo change path
+          />
+        </div>
+        <div className="grid-item" onClick={() => navigateToPage('/rooms')}>
+          <SpotCard
+            component={component}
+            height={height}
+            image={image}
+            onClick={() => navigateToPage('/rooms')} // todo change path
+          />
+        </div>
+        <div className="grid-item" onClick={() => navigateToPage('/rooms')}>
+          <SpotCard
+            component={component}
+            height={height}
+            image={image}
+            onClick={() => navigateToPage('/rooms')} // todo change path
+          />
+        </div>
+        <div className="grid-item" onClick={() => navigateToPage('/rooms')}>
+          <SpotCard
+            component={component}
+            height={height}
+            image={image}
+            onClick={() => navigateToPage('/rooms')} // todo change path
+          />
+        </div>
+        <div className="grid-item" onClick={() => navigateToPage('/rooms')}>
+          <SpotCard
+            component={component}
+            height={height}
+            image={image}
+            onClick={() => navigateToPage('/rooms')} // todo change path
+          />
+        </div>
       </div>
 
     </div>
