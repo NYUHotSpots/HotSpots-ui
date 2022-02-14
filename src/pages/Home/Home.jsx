45e024858a91a0d68849978aa09c56c2c1c9a734
Kevin Chen <kc3585@nyu.edu>
2022-02-14 13:59:35 +0000

    implementing first authentication

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
-import {useHistory} from 'react-router-dom';
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
-export default function Home(){
+import Profile from "../Authentication/Profile";
+
+export default function Home() {
   const history = useHistory();
+  const { loginWithRedirect, logout } = useAuth0();
 
   function navigateToPage(path) {
     history.push(path);
@@ -10,67 +14,66 @@ export default function Home(){
 
   return (
     <div className="content">
+      <button onClick={() => loginWithRedirect()}>Log In</button>
+      <button onClick={() => logout({ returnTo: window.location.origin })}>
+        Log Out
+      </button>
+      <Profile />
       <h1>Hotspots</h1>
       <div>
-        <p className="quote">"Crowdsourcing study spaces for students by students"</p>
-        <p className="dev-names">Tiffany Chan, Kevin Chen, Kevin Iza, Kathy Pan</p>
+        <p className="quote">
+          "Crowdsourcing study spaces for students by students"
+        </p>
+        <p className="dev-names">
+          Tiffany Chan, Kevin Chen, Kevin Iza, Kathy Pan
+        </p>
       </div>
 
-      { 
+      {
         // button for Locations
       }
-      <button
-        onClick={() => navigateToPage('/rooms')}
-        className="page-button"
-      >
+      <button onClick={() => navigateToPage("/rooms")} className="page-button">
         View All Locations
       </button>
 
-      { 
+      {
         // button for User login?
       }
-      <button
-        onClick={() => navigateToPage('/login')}
-        className="page-button"
-      >
+      <button onClick={() => navigateToPage("/login")} className="page-button">
         User Login
-      </button> 
+      </button>
 
-      { /*
+      {/*
         - button for User signup?
         - maybe we can split the pages where the user logs in and signs up
         - currently routes to the same place as User Login
       */}
-      <button
-        onClick={() => navigateToPage('/users')}
-        className="page-button"
-      >
+      <button onClick={() => navigateToPage("/users")} className="page-button">
         User SignUp
-      </button> 
+      </button>
 
-      { /*
+      {/*
         - button to Add Review
         - can be changed to show in the Locations page instead
         - currently redirects to /rooms
       */}
       <button
-        onClick={() => navigateToPage('/createreview')}
+        onClick={() => navigateToPage("/createreview")}
         className="page-button"
       >
         Add a Review
-      </button> 
+      </button>
 
-      { /*
+      {/*
         - button to Look at all the Reviews (Can be Change to just a room)
         - currently redirects to /rooms
       */}
       <button
-        onClick={() => navigateToPage('/checkreviews')}
+        onClick={() => navigateToPage("/checkreviews")}
         className="page-button"
       >
         Check Reviews
-      </button> 
+      </button>
     </div>
   );
-};
-
+}
