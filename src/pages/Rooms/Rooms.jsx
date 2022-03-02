0031da4161d72f71b6c18d9082b643f6e86981ec
Kevin Chen <kc3585@nyu.edu>
2022-03-02 14:46:56 +0000

    call spot details api to retrieve specific spot details

--- src/pages/Rooms/Rooms.jsx
+++ src/pages/Rooms/Rooms.jsx
@@ -24,24 +24,28 @@ export default function Rooms() {
 
   useEffect(() => {
     console.log('Before Axios, spotID = ' + spotID);
-    axios.get('https://hotspotsapi.herokuapp.com/spot/list') // Gets the spots from our api
-    //axios.get('https://hotspotsapi.herokuapp.com/spot/${spot_id}') // link for specific spot, doesn't work
 
-      .then((response) => {     //Waits for the response of the .get, once it gets the values it does below
-        console.log(response.data);
-        console.log('getting values'); // Remove later, testing
-        if (response.data){
-          console.log('api sent data');// Remove later, testing
-          setSpots(response.data);
-          console.log("Retrieved spots from DB", spots);
-        }
-        console.log('At the end');// Remove later, testing
-      })
-      .catch(error => {       //If an error is thrown this catch puts up a warning instead
-        console.log(error);
-        console.log('You didnt get the value RIPS'); // Remove later, testing
-        setError(error);
-      });
+    getSpotDetails();
+
+    // axios.get('https://hotspotsapi.herokuapp.com/spot/list') // Gets the spots from our api
+    // //axios.get('https://hotspotsapi.herokuapp.com/spot/${spot_id}') // link for specific spot, doesn't work
+
+    //   .then((response) => {     //Waits for the response of the .get, once it gets the values it does below
+    //     console.log(response.data);
+    //     console.log('getting values'); // Remove later, testing
+    //     if (response.data){
+    //       console.log('api sent data');// Remove later, testing
+    //       setSpots(response.data);
+    //       console.log("Retrieved spots from DB", spots);
+    //     }
+    //     console.log('At the end');// Remove later, testing
+    //   })
+    //   .catch(error => {       //If an error is thrown this catch puts up a warning instead
+    //     console.log(error);
+    //     console.log('You didnt get the value RIPS'); // Remove later, testing
+    //     setError(error);
+    //   });
+
   }, [refresh, spotID, spots]);
 
   const getSpotDetails = () => {
@@ -119,17 +123,15 @@ export default function Rooms() {
       </div>
 
            {/*Sets up the 'table' with details*/}
-        <h3 classname = "detail-subheaders"> Availability </h3>
+        <h3 className = "detail-subheaders"> Availability </h3>
         <span>tester</span>
         
-        <h3 classname = "detail-subheaders"> Noise Level </h3>
+        <h3 className = "detail-subheaders"> Noise Level </h3>
 
-        <h3 classname = "detail-subheaders"> Ambience </h3>
+        <h3 className = "detail-subheaders"> Ambience </h3>
 
-        <h3 classname = "detail-subheaders"> Temperature </h3>
+        <h3 className = "detail-subheaders"> Temperature </h3>
       
-
-
       <div>     {/*A button to add a room*/}
         <button className="page-button" onClick={() => setIsModalOpen(true)}> Add New Location </button>
       </div>
