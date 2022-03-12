452f85b87091e82a9463d64f63fc3ad5786c2873
KathyP825 <kp2327@nyu.edu>
2022-03-11 20:33:40 -0500

    added fill-in forms & CSS styling to CreateReview

--- src/pages/CreateReview/CreateReview.jsx
+++ src/pages/CreateReview/CreateReview.jsx
@@ -52,6 +52,9 @@ export default function CreateReview() {
         <button onClick={() => history.push('/')} className="page-button"> {"<-- "}Go Back Home </button>
       </div>
 
+      <h2>Location Title</h2>
+      <h3>Location Address</h3>
+
       <div className="contents">
         <button onClick={() => createReview()} className="page-button"> createReview() Button </button> 
       </div>
@@ -76,10 +79,67 @@ export default function CreateReview() {
 
       <input
         className="createreview-input"
-        placeholder="Review Name"
+        placeholder="Review Title"
         value={newReviewName}
         onChange={(e) => newReviewName(e.target.value)}
       />
+
+      <div className="createreview-input">
+        <form>
+          <label className="item-styling"> <big><b>Review Title: </b></big> </label>
+          <input type="text" name="Review Title" placeholder="Great Location!" /><br></br><br></br>
+
+          <label className="item-styling"> <big><b>Rating out of 5: </b></big> </label>
+          <input type="text" name="rating" placeholder="5" /><br></br><br></br>
+
+          
+          <p><strong><big>Availability:</big></strong></p>
+          <input type="radio" id="noAvail" name="availability" value="1"></input>
+          <label for="noAvail"> At capacity (No seats)</label><br></br>
+          <input type="radio" id="limitedAvail" name="availability" value="2"></input>
+          <label for="limitedAvail"> Crowded (limited seating)</label><br></br>
+          <input type="radio" id="someAvail" name="availability" value="3"></input>
+          <label for="someAvail"> Not too crowded (Some seats available)</label><br></br>
+          <input type="radio" id="manyAvail" name="availability" value="4"></input>
+          <label for="manyAvail"> Many seats</label><br></br><br></br>
+          
+          <p><strong><big>Noise Level:</big></strong></p>
+          <input type="radio" id="quiet" name="noise" value="1"></input>
+          <label for="html"> Quiet (silent / near silent)</label><br></br>
+          <input type="radio" id="someNoise" name="noise" value="2"></input>
+          <label for="html"> Some noise (whispering to normal speech)</label><br></br>
+          <input type="radio" id="noise" name="noise" value="3"></input>
+          <label for="html"> Noisy (very loud)</label><br></br><br></br>
+
+          <p><strong><big>Temperature:</big></strong></p>
+          <input type="radio" id="cold" name="temp" value="1"></input>
+          <label for="html"> Cold / Chilly</label><br></br>
+          <input type="radio" id="comfortable" name="temp" value="2"></input>
+          <label for="html"> Comfortable</label><br></br>
+          <input type="radio" id="warm" name="temp" value="3"></input>
+          <label for="html"> Warm / Hot</label><br></br><br></br>
+
+          <p><strong><big>Ambiance:</big></strong></p>
+          <input type="radio" id="calm" name="ambi" value="1"></input>
+          <label for="html"> Calm </label><br></br>
+          <input type="radio" id="busy" name="ambi" value="2"></input>
+          <label for="html"> Busy</label><br></br>
+
+
+          
+          <br></br><br></br>
+          <label> <big><b>Comments: </b></big> </label>
+          <textarea></textarea><br></br><br></br>
+
+          
+          
+
+          
+          <input className="submit-button" type="submit" value="Submit" />
+        </form>
+      </div>
+
+
     </div> //Content div
   );
 }
