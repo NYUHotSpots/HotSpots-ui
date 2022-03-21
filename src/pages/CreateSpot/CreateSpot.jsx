2d7f10e87fc82723d525ce61cdc9fbd4c5713999
Kevin Chen <kc3585@nyu.edu>
2022-03-21 17:01:01 +0000

    call create spot api

--- src/pages/CreateSpot/CreateSpot.jsx
+++ src/pages/CreateSpot/CreateSpot.jsx
@@ -3,7 +3,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { useHistory } from "react-router-dom";
 import axios from "axios";
 
-import './createspot.css';
+import "./createspot.css";
 
 export default function CreateSpot() {
   const history = useHistory();
@@ -15,26 +15,31 @@ export default function CreateSpot() {
   //   history.push(path);
   // }
 
-  const createSpot = () => {
-    const sendRequest = async () => {
+  const createSpot = (spotName, spotImage, spotAddress, spotCapacity) => {
+    console.log("In createSpot function: ", spotName, spotImage, spotAddress, spotCapacity);
+    const sendRequest = async (spotName, spotImage, spotAddress, spotCapacity) => {
       try {
         const accessToken = await getAccessTokenSilently();
         console.log(accessToken);
 
+        const body = {
+          spotName: spotName,
+          spotImage: spotImage,
+          spotAddress: spotAddress,
+          spotCapacity: spotCapacity,
+        };
+
         const config = {
           headers: {
-            "content-type": "application/json",
-            Authorization: `Bearer ${accessToken}`,
-          },
-          body: {
-            spotName: "",
-            spotImage: "",
-            spotAddress: "",
-            spotCapacity: "",
-          },
+            // "content-type": "application/x-www-form-urlencoded",
+            "Authorization": `Bearer ${accessToken}`,
+            // "accept": "application/json"
+          }
         };
+
         const response = await axios.post(
           `${apiServerUrl}/spots/create`,
+          body,
           config
         );
 
@@ -45,48 +50,86 @@ export default function CreateSpot() {
       }
     };
 
-    sendRequest();
+    sendRequest(spotName, spotImage, spotAddress, spotCapacity);
   };
 
   return (
     <div className="content">
       <div className="rooms-header">
         <h1>Add a Spot</h1>
-        <button onClick={() => history.push('/')} className="page-button"> {"<-- "}Go Back Home </button>
+        <button onClick={() => history.push("/")} className="page-button">
+          {" "}
+          {"<-- "}Go Back Home{" "}
+        </button>
 
-        <button onClick={() => createSpot()} className="page-button"> createSpot() Button </button> 
+        {/* <button onClick={() => createSpot()} className="page-button"> createSpot() Button </button>  */}
         {/* don't know why button is clicking back to this page, is here to remove warning error
         I assume this is the submit button?
         */}
-
       </div>
-
 
       <div className="contents">
-        <form>
-          <label className="item-styling"> <big><b>Spot / Location Name: </b></big> </label>
-          <input type="text" name="Spot Name" placeholder="NYU Makerspace" /><br></br><br></br>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            const elements = e.target.elements;
+            createSpot(elements.spotName.value, elements.spotImage.value, elements.spotAddress.value, elements.spotCapacity.value);
+            // console.log(e.target.elements.spotAddress.value);
+            // console.log(e.target.elements.username.value); // from elements property
+            // console.log(e.target.username.value); // or directly
+          }}
+        >
+          <label className="item-styling">
+            {" "}
+            <big>
+              <b>Spot / Location Name: </b>
+            </big>{" "}
+          </label>
+          <input type="text" name="spotName" placeholder="NYU Makerspace" />
+          <br></br>
+          <br></br>
 
-          <label className="item-styling"> <big><b>Address: </b></big> </label>
-          <input type="text" name="address" placeholder="6 MetroTech Center" /><br></br><br></br>
+          <label className="item-styling">
+            {" "}
+            <big>
+              <b>Address: </b>
+            </big>{" "}
+          </label>
+          <input
+            type="text"
+            name="spotAddress"
+            placeholder="6 MetroTech Center"
+          />
+          <br></br>
+          <br></br>
 
-          <label className="item-styling"> <big><b>Capacity: </b></big> </label>
-          <input type="text" name="capacity" placeholder="100" /><br></br><br></br>
+          <label className="item-styling">
+            {" "}
+            <big>
+              <b>Capacity: </b>
+            </big>{" "}
+          </label>
+          <input type="text" name="spotCapacity" placeholder="100" />
+          <br></br>
+          <br></br>
 
-          <label className="item-styling"> <big><b>Image Link: </b></big> </label>
-          <input type="text" name="img-link" placeholder="https://example.com/" /><br></br><br></br>
-          
+          <label className="item-styling">
+            {" "}
+            <big>
+              <b>Image Link: </b>
+            </big>{" "}
+          </label>
+          <input
+            type="text"
+            name="spotImage"
+            placeholder="https://example.com/"
+          />
+          <br></br>
+          <br></br>
 
-          
           <input className="submit-button" type="submit" value="Submit" />
         </form>
-
       </div>
-
-
     </div>
-    
-
-    
   );
 }
