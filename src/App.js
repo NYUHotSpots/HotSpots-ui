6811457e3eaccf053f87aa8e01f5bede10d97982
Kevin Chen <kc3585@nyu.edu>
2022-04-04 21:09:14 +0100

    add hashrouter for github pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
 
 import Home from './pages/Home/Home';
 import SpotInfo from './pages/SpotInfo/SpotInfo';
