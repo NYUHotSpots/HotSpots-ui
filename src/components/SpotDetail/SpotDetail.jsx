34164bb224e27dd8546f1a7957b233c5cdb2639a
Izakevin <keviniza64@gmail.com>
2022-04-21 19:14:04 -0400

    Updated spotdetails to show a message for the location, included the capacity in spotdetails

--- src/components/SpotDetail/SpotDetail.jsx
+++ src/components/SpotDetail/SpotDetail.jsx
@@ -7,7 +7,76 @@ import './spotdetail.css';
 export default function SpotItem({name, image, address, capacity, availability, noise, temp, ambi, reviews}) {
   const history = useHistory();
 
+  // For availability
   console.log(availability);
+  if(availability <= 1){
+    availability = "At Capacity";
+  }
+  else if (availability > 1 && availability <= 2){
+    availability = "Crowded (limited seating)";
+  }
+  else if (availability > 2 && availability <= 3){
+    availability = "Not too crowded";
+  }
+  else if (availability > 3 && availability <= 4){
+    availability = "Many seats Available";
+  }
+  else{
+    availability = "Too mant seats free";
+  }
+
+  // For Noise Level
+  if(noise <= 1){
+    noise = "Quiet";
+  }
+  else if (noise > 1 && noise <= 2){
+    noise = "Some noise";
+  }
+  else if (noise > 2 && noise <= 3){
+    noise = "Noisy";
+  }
+  else if (noise > 3 && noise <= 4){
+    noise = "Unbearably loud";
+  }
+  else{
+    noise = "Ruptured eardrum";
+  }
+
+  // For ambience
+  if(ambi <= 1){
+    ambi = "Chill";
+  }
+  else if (ambi > 1 && ambi <= 2){
+    ambi = "Regular";
+  }
+  else if (ambi > 2 && ambi <= 3){
+    ambi = "Neutral";
+  }
+  else if (ambi > 3 && ambi <= 4){
+    ambi = "Busy";
+  }
+  else{
+    ambi = "Serious Business";
+  }
+
+  // For tempature
+  if(temp <= 1){
+    temp = "Cold/ Chilly";
+  }
+  else if (temp > 1 && temp <= 2){
+    temp = "Comfortable";
+  }
+  else if (temp > 2 && temp <= 3){
+    temp = "warm";
+  }
+  else if (temp > 3 && temp <= 4){
+    temp = "Flaming";
+  }
+  else{
+    temp = "Burning";
+  }
+
+  console.log(capacity);
 
   return (
     <div className="spot-details">
@@ -40,6 +109,7 @@ export default function SpotItem({name, image, address, capacity, availability, 
             <li>Noise Level: {noise}</li>
             <li>Ambience: {ambi}</li>
             <li>Temperature: {temp}</li>
+            <li>Capacity: {capacity}</li>
           </ul>
         </div>
       </div>
