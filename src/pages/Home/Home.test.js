b61defa0c63990300fd2d328f26f9e6b5f2397e9
Kevin Chen <kc3585@nyu.edu>
2022-04-26 15:16:11 +0100

    write Home component jest snapshot tests

--- src/pages/Home/Home.test.js
+++ src/pages/Home/Home.test.js
@@ -1,16 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import renderer from "react-test-renderer";
-import App from "./App";
+import Home from "./Home";
 
 describe("App Component", () => {
   test("renders learn react link", () => {
-    render(<App />);
+    render(<Home />);
     const linkElement = screen.getByText(/HotSpots/i);
     expect(linkElement).toBeInTheDocument();
   });
 
-  test("App snapshots", () => {
-    const tree = renderer.create(<App />).toJSON();
+  test("Home snapshots", () => {
+    const tree = renderer.create(<Home />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
